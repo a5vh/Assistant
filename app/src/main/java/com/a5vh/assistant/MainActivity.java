6583da72a0ea9b9bae0a3f219e19d75ae5e8c3c0
August H. Hummert V <august.hummert5@gmail.com>
2019-02-26 13:20:32 -0600

    2/26
    colors still

--- app/src/main/java/com/a5vh/assistant/MainActivity.java
+++ app/src/main/java/com/a5vh/assistant/MainActivity.java
@@ -1,5 +1,6 @@
 package com.a5vh.assistant;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
